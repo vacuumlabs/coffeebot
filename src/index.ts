9d26a458927450b770ede30f51ecced36473e932
rtrembecky <rtrembecky@gmail.com>
2022-03-08 00:26:32 +0100

    simple bolt app setup as of docs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-const code = async () => {}
+import {App} from '@slack/bolt'
 
-code()
+// source: https://slack.dev/bolt-js/tutorial/getting-started#setting-up-your-project
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+})
+
+;(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000)
+
+  console.log('⚡️ Bolt app is running!')
+})()
