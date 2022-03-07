481292791368aaae3a7f483a6d6202c165a03423
rtrembecky <rtrembecky@gmail.com>
2022-03-08 00:00:47 +0100

    init a typescript project from another project of mine. features: - typescript - yarn 3 with plugins and using PnP - eslint + prettier - build, start, dev, and lint scripts

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    // Baseline configurations
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+
+    // Disable ESLint rules conflicting with Prettier
+    'prettier',
+    'prettier/prettier',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-unused-vars': 'off',
+  },
+}
