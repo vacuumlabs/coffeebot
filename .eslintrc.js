6de7356b48590c2e2266a95111c111e84c05d93b
rtrembecky <rtrembecky@gmail.com>
2022-03-28 00:54:08 +0200

    eslint ignore build

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  ignorePatterns: 'build',
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     // Baseline configurations
